8a4e3f18efc5d92f9766e5aad9b5e4506add3c3c
Andy Gayton <andy@thecablelounge.com>
2012-05-09 17:19:06 -0700

    update package_dir

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version='0.0.4',
     author='Andy Gayton',
     author_email='andy@simperium.com',
+    package_dir = {'': 'python'},
     packages=['simperium', 'simperium.test'],
     scripts=[],
     # url='http://pypi.python.org/pypi/Simperium/',
