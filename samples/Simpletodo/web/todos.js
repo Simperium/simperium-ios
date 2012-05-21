0639344f456c0277a837e16348fe3c52f884f403
Fred Cheng <fred@ecksor.com>
2012-05-21 16:29:25 -0700

    fix todo display in simpletodo web sample

--- samples/Simpletodo/web/todos.js
+++ samples/Simpletodo/web/todos.js
@@ -27,7 +27,7 @@ $(function(){
 
     // Ensure that each todo created has `title`.
     initialize: function() {
-      if (!this.get("title")) {
+      if (!this.has("title")) {
         this.set({"title": this.defaults().title});
       }
     },
@@ -116,7 +116,7 @@ $(function(){
     // Re-render the titles of the todo item.
     render: function() {
       this.$el.html(this.template(this.model.toJSON()));
-      this.$el.toggleClass('done', this.model.get('done'));
+      this.$el.toggleClass('done', this.model.get('done') ? true : false);
       this.input = this.$('.edit');
       return this;
     },
