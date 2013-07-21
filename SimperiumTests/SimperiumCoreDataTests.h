51a902ff56b092860c5e3e142e9da9d4e1cc88a2
Jorge Leandro Perez <jleandroperez@gmail.com>
2013-07-21 11:55:21 -0300

    Adding Simperium CoreData Tests

---
+++ SimperiumTests/SimperiumCoreDataTests.h
@@ -0,0 +1,13 @@
+//
+//  SimperiumCoreDataTests.h
+//  Simperium
+//
+//  Created by Jorge Leandro Perez on 7/20/13.
+//  Copyright (c) 2013 Simperium. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SimperiumCoreDataTests : SimperiumTests
+
+@end
---
+++ SimperiumTests/SimperiumCoreDataTests.m
@@ -0,0 +1,13 @@
+//
+//  SimperiumCoreDataTests.m
+//  Simperium
+//
+//  Created by Jorge Leandro Perez on 7/20/13.
+//  Copyright (c) 2013 Simperium. All rights reserved.
+//
+
+#import "SimperiumCoreDataTests.h"
+
+@implementation SimperiumCoreDataTests
+
+@end
