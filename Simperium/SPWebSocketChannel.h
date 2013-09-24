a8e558b5c5f26cef14af2e59b9a4e252902381e6
Jorge Leandro Perez <jleandroperez@gmail.com>
2013-09-24 11:08:47 -0300

    Updating SPWebSocketChannel to use properties

--- Simperium/SPWebSocketChannel.h
+++ Simperium/SPWebSocketChannel.h
@@ -14,27 +14,14 @@
 @protocol SPDiffable;
 
 @interface SPWebSocketChannel : NSObject
-{
-    BOOL indexing;
-    BOOL started;
-    BOOL retrievingObjectHistory;
-    int retryDelay;
-    NSString *nextMark;
-    NSMutableArray *indexArray;
-    NSString *pendingLastChangeSignature;
-    SPWebSocketInterface *__weak webSocketManager;
-    NSString *name;
-    int number;
-    NSInteger objectVersionsPending;
-}
 
 @property (nonatomic, weak) SPWebSocketInterface *webSocketManager;
-@property (nonatomic, copy) NSString *nextMark;
 @property (nonatomic, strong) NSMutableArray *indexArray;
+@property (nonatomic, copy) NSString *nextMark;
 @property (nonatomic, copy) NSString *pendingLastChangeSignature;
-@property (nonatomic) int number;
 @property (nonatomic, copy) NSString *name;
-@property (nonatomic) BOOL started;
+@property (nonatomic, assign) NSInteger number;
+@property (nonatomic, assign) BOOL started;
 
 + (void)setNetworkActivityIndicatorEnabled:(BOOL)enabled;
 - (id)initWithSimperium:(Simperium *)s clientID:(NSString *)cid;
