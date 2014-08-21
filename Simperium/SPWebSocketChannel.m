8e2e975e626e462c67da6d82c16307897590078a
Jorge Leandro Perez <jorge@lantean.co>
2014-08-20 22:59:42 -0300

    Nukes legacy changes upon rebase

--- Simperium/SPWebSocketChannel.m
+++ Simperium/SPWebSocketChannel.m
@@ -603,7 +603,8 @@ typedef void(^SPWebSocketSyncedBlockType)(void);
         }
         
         [bucket.indexProcessor processVersions:batch bucket:bucket changeHandler:^(NSString *key) {
-            // Local version was different, so process it as a local change
+            // Local version was different, so nuke old changes, and recalculate the delta
+            [bucket.changeProcessor discardPendingChanges:key bucket:bucket];
             [bucket.changeProcessor enqueueObjectForMoreChanges:key bucket:bucket];
         }];
         
