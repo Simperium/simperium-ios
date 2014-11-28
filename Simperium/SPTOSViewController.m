2c06b4353a0ea382d5bfff93e2ed41fbc08e7e46
iPaddy <prvink@prv.nl>
2014-11-28 10:15:29 +0100

    Added localized custom TOS URL

--- Simperium/SPTOSViewController.m
+++ Simperium/SPTOSViewController.m
@@ -6,8 +6,6 @@
 //  Copyright (c) 2013 Simperium. All rights reserved.
 //
 
-NSString *const TOSUrl = @"http://simperium.com/tos/";
-
 #import "SPTOSViewController.h"
 
 @interface SPTOSViewController ()
@@ -42,16 +40,23 @@ NSString *const TOSUrl = @"http://simperium.com/tos/";
     
     self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(dismissAction:)];
     
-    NSURLRequest *request = [[NSURLRequest alloc] initWithURL:[NSURL URLWithString:TOSUrl]];
+    NSString *termsURL = NSLocalizedString(@"TOS URL", @"Using this localized string you can set your own TOS per language");
+    
+    if (![self isValidURL:termsURL]) {
+        // Show default Simperium TOS
+        termsURL = @"http://simperium.com/tos/";
+    }
+    
+    NSURLRequest *request = [[NSURLRequest alloc] initWithURL:[NSURL URLWithString:termsURL]];
     [webView loadRequest:request];
 }
 
-#pragma mark UIWebViewDelegate Methods
-
-- (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType {
-    
-    return [[NSString stringWithFormat:@"%@", request.URL] isEqualToString:TOSUrl];
+- (BOOL)isValidURL:(NSString *)urlString{
+    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:urlString]];
+    return [NSURLConnection canHandleRequest:request];
 }
+
+#pragma mark UIWebViewDelegate Methods
 
 - (void)webViewDidStartLoad:(UIWebView *)webView {
     
