396c888b2055d5c81d00032a08e50d567d52530b
Jorge Leandro Perez <jorge@lantean.co>
2017-11-30 14:12:44 -0300

    Increasing Version to Mark 0.8.20

--- Simperium/SPEnvironment.m
+++ Simperium/SPEnvironment.m
@@ -30,7 +30,7 @@ NSString* const SPLibraryID = @"osx";
 #endif
 
 // TODO: Update this automatically via a script that looks at current git tag
-NSString* const SPLibraryVersion = @"0.8.19";
+NSString* const SPLibraryVersion = @"0.8.20";
 
 /// SSL Pinning
 ///
