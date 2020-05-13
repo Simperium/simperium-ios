c8af47353805bffa0760af700f7a0bb2ad0e8270
Jorge Leandro Perez <jorge.perez@automattic.com>
2020-05-13 19:15:50 -0300

    Updates version to Mark 0.8.27

--- Simperium/SPEnvironment.m
+++ Simperium/SPEnvironment.m
@@ -30,7 +30,7 @@ NSString* const SPLibraryID = @"osx";
 #endif
 
 // TODO: Update this automatically via a script that looks at current git tag
-NSString* const SPLibraryVersion = @"0.8.26";
+NSString* const SPLibraryVersion = @"0.8.27";
 
 /// SSL Pinning
 ///
