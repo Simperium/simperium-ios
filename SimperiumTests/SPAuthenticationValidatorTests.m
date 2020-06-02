c23da8e35b857a508f8d7a750f8b8a0938eabc03
Jorge Leandro Perez <jorge.perez@automattic.com>
2020-06-02 00:42:14 -0300

    Implements SPAuthenticationValidatorTests

---
+++ SimperiumTests/SPAuthenticationValidatorTests.m
@@ -0,0 +1,67 @@
+#import <XCTest/XCTest.h>
+#import "XCTestCase+Simperium.h"
+#import "SPAuthenticationValidator.h"
+
+@interface SPAuthenticationValidatorTests : XCTestCase
+@property (nonatomic, strong) SPAuthenticationValidator *validator;
+@end
+
+@implementation SPAuthenticationValidatorTests
+
+- (void)setUp {
+    self.validator = [SPAuthenticationValidator new];
+}
+
+- (void)testPerformUsernameValidationReturnsTrueWheneverInputEmailIsValid {
+    NSArray *emails = @[
+        @"j@j.com",
+        @"something@simplenote.blog",
+        @"something@simplenote.blog.ar"
+    ];
+
+    for (NSString *email in emails) {
+        NSError *error = nil;
+        BOOL isValid = [self.validator validateUsername:email error:&error];
+
+        XCTAssertTrue(isValid);
+        XCTAssertNil(error);
+    }
+}
+
+- (void)testValidateUsernameReturnsErrorWheneverInputStringIsShorterThanExpected {
+    NSString *username = @"somethinghere";
+    NSError *error = nil;
+    BOOL isValid = [self.validator validateUsername:username error:&error];
+
+    XCTAssertFalse(isValid);
+    XCTAssertEqual(error.code, SPAuthenticationErrorsEmailInvalid);
+}
+
+- (void)testValidatePasswordReturnsErrorWheneverPasswordMatchesUsername {
+    NSString *username = @"somethinghere";
+    NSError *error = nil;
+    BOOL isValid = [self.validator validatePasswordWithUsername:username password:username error:&error];
+
+    XCTAssertFalse(isValid);
+    XCTAssertEqual(error.code, SPAuthenticationErrorsPasswordMatchesUsername);
+}
+
+- (void)testValidatePasswordReturnsErrorWheneverPasswordContainsInvalidCharacters {
+    NSString *username = @"somethinghere";
+    NSArray *passwords = @[
+        @"\t12345678",
+        @"\n12345678",
+        @"1234\n5678\t",
+        @"12345678\t"
+    ];
+
+    for (NSString *password in passwords) {
+        NSError *error = nil;
+        BOOL isValid = [self.validator validatePasswordWithUsername:username password:password error:&error];
+
+        XCTAssertFalse(isValid);
+        XCTAssertEqual(error.code, SPAuthenticationErrorsPasswordContainsInvalidCharacter);
+    }
+}
+
+@end
