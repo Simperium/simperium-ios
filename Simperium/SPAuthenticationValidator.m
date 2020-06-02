ac9e7e2f1757998f2be63d23e86f516a8059b4b7
Jorge Leandro Perez <jorge.perez@automattic.com>
2020-06-02 15:58:49 -0300

    SPAuthenticationValidator: Documenting API

--- Simperium/SPAuthenticationValidator.m
+++ Simperium/SPAuthenticationValidator.m
@@ -107,6 +107,13 @@ NSString* const SPAuthenticationErrorDomain = @"SPAuthenticationValidatorDomain"
 }
 
 - (BOOL)mustPerformPasswordResetWithUsername:(NSString *)username password:(NSString *)password {
+    //  Note:
+    //   1. Is the password **now** considered unsafe?
+    //   2. Was the password **then** considered safe?
+    //   3. Is the password valid (does the backend let us thru?)
+    //
+    //  If all of the above are true, we'll require the Password to be Reset
+    //
     BOOL isValidUsername = [self validateUsername:username error:nil];
     BOOL isValidLegacyPassword = (password.length >= self.legacyMinimumPasswordLength);
     BOOL isValidStrongPassword = [self validatePasswordWithUsername:username password:password error:nil];
