82109fbb40c7ce131c28885dbd7a0dac5953785c
Jorge Leandro Perez <jorge.perez@automattic.com>
2021-08-14 01:24:19 -0300

    Restores Authenticated check on Logout

--- Simperium/Simperium.m
+++ Simperium/Simperium.m
@@ -587,7 +587,12 @@ static SPLogLevels logLevel                     = SPLogLevelsInfo;
 #pragma mark ====================================================================================
 
 - (void)signOutAndRemoveLocalData:(BOOL)remove completion:(SimperiumSignoutCompletion)completion {
-        
+
+    // Don't proceed, if the user isn't logged in
+    if (!self.user.authenticated) {
+        return;
+    }
+
     if (self.logoutInProgress) {
         SPLogError(@"Error: Simperium is already logging out");
         return;
